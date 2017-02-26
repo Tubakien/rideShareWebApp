7bdee173aa093fbb86619c1b22e3a2a739ca390c
Tubakien <tubakien@gmail.com>
2017-02-26 17:35:48 -0600

    Starting to implement DAOs and Hibernate.

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,7 @@
+package com.entity;
+
+/**
+ * Created by student on 2/26/17.
+ */
+public class User {
+}
---
+++ src/main/java/com/persistence/UserDao.java
@@ -0,0 +1,7 @@
+package com.persistence;
+
+/**
+ * Created by student on 2/26/17.
+ */
+public class UserDao {
+}
