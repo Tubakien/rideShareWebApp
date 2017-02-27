46424301b20eb1b05391e338f3f7f4d35f97f2ae
Tubakien <tubakien@gmail.com>
2017-02-26 18:12:55 -0600

    Finished coding User DAO. TODO: Test

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -1,7 +1,110 @@
 package com.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
 /**
  * Created by student on 2/26/17.
  */
+@Entity
+@Table(name="user")
 public class User {
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name="home_address_id")
+    private int homeAddressId;
+
+    @Column(name = "phone")
+    private int phoneNumber;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    @Column(name="user_id")
+    private int userId;
+
+    public User() {
+    }
+
+    public User(String username, String email, int phone) {
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getHomeAddressId() {
+        return homeAddressId;
+    }
+
+    public void setHomeAddressId(int homeAddressId) {
+        this.homeAddressId = homeAddressId;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", homeAddressId=" + homeAddressId +
+                ", phoneNumber=" + phoneNumber +
+                ", userId=" + userId +
+                '}';
+    }
 }
--- src/main/java/com/persistence/UserDao.java
+++ src/main/java/com/persistence/UserDao.java
@@ -1,7 +1,58 @@
 package com.persistence;
 
+import com.entity.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by student on 2/26/17.
  */
 public class UserDao {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    /** Return a list of all users
+     *
+     * @return All users
+     */
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        users = session.createCriteria(User.class).list();
+        return users;
+    }
+
+    /**
+     * add a user
+     *
+     * @param user
+     * @return the id of the inserted record.
+     */
+    public int addUser(User user) {
+        Session session = null;
+        int id = 0;
+
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            Transaction trans = session.beginTransaction();
+            id = (int) session.save(user); // INSERT statement
+            trans.commit();
+        } catch (HibernateException he) {
+            log.error("HibernateException: " + he);
+        } catch (Exception e) {
+            log.error("Exception: " + e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return id;
+    }
 }
