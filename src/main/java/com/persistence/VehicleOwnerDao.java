189f60c1abddcd2f15033d78472300ca3ae8dc51
Tubakien <tubakien@gmail.com>
2017-04-12 12:12:37 -0500

    Work startred after group project

---
+++ src/main/java/com/persistence/VehicleOwnerDao.java
@@ -0,0 +1,14 @@
+package com.persistence;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by student on 4/11/17.
+ */
+public class VehicleOwnerDao {
+    private final Logger log = Logger.getLogger(this.getClass());
+    public int addVehicleOwner(String license, String driversLicense){
+
+        return 0;
+    }
+}
